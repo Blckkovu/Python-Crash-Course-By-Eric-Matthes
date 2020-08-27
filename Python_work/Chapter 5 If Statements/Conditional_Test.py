23cd4f3be9282c969fdeaf6a5993a7ec4a9e6fd4
Blckkovu <jamesmurethimwangi@gmail.com>
2020-08-27 13:41:05 +0300

    commit if tests

--- Python_work/Chapter 5 If Statements/Conditional_Test.py
+++ Python_work/Chapter 5 If Statements/Conditional_Test.py
@@ -1,5 +1,87 @@
-car = 'subaru'
-print("Is car == 'subaru'? I predict True.")
-print(car == 'subaru')
-print("\nIs car == 'audi'? I predict False.")
-print(car == 'audi')
+#exercise 5.1
+#Writing a series of conditional test
+Deity= "Thoth"
+if Deity== 'Thoth':
+    print('Wisdom')
+else:
+    print('Wrong deity')
+
+
+polarity="North"
+if polarity == "South":
+    print('Face South')
+else:
+    print("Face North")
+
+#or
+vibration1= 176
+vibration2= 160
+if vibration1 > 174 or vibration2 > 174 :
+    print("Your good")
+else:
+    print("Please Meditate")
+    #or ust checks if one condition is true
+
+#and
+vibration1= 174
+vibration2= 160
+if vibration1 > 174 and vibration2 > 174 :
+    print("Your good")
+else:
+    print("Please Meditate")
+    #and checks if both conditions are both true or false
+
+
+#exercise 5.2
+#test for equality
+Car = "Tesla"
+if Car == "Tesla":
+    print (True)
+else:
+    print(False)
+
+#test for inequality
+Car = "Tesla"
+if Car != "Tesla":
+    print (True)
+else:
+    print(False)
+
+#Numerical test
+Number=10
+if Number == 10:
+    print ('exact')
+
+Number=10
+if Number < 10:
+    print ('less')
+
+Number=11
+if Number > 10:
+    print ('Great')
+
+Number=12
+if Number >= 10:
+    print ('perfect')
+
+#Tests using the and keyword and the or keyword
+Number=100
+if Number > 50 or Number < 90:
+    print ('passed')
+
+Number=100
+Number1=90
+if Number > 70 and Number1 < 92:
+    print ('passed')
+else:
+    print('failed')
+
+#test whether an item is in a list
+arr = [1,2,3,4,5]
+if 1 in arr:
+    print(True)
+
+if 7 in arr:
+    print(True)
+else:
+    print(False)
---
+++ Python_work/Chapter 5 If Statements/If_Statement_AlienExercise.py
@@ -0,0 +1,53 @@
+#Alien color 5.3
+Alien_color=['Green', 'yellow', 'red']
+if 'Green' in Alien_color:
+    print('5 points earned')
+
+#iftest
+if "yellow" in Alien_color:
+    print("30 points earned")
+#this version has no output
+if 'blue' in Alien_color:
+    print('100 points earned')
+
+#exercise 5.4
+Alien_color= "Green"
+if Alien_color == 'Green':
+    print('5 points earned')
+else:
+    Alien_color != 'Green'
+    print('10 points earned')
+
+Alien_color= "Red"
+if Alien_color == 'Green':
+    print('5 points earned')
+else:
+    Alien_color != 'Green'
+    print('10 points earned')
+
+#exercise 5.5
+Alien_color=['Green', 'yellow', 'red']
+if 'Green' in Alien_color:
+    print('5 points earned')
+elif 'yellow' in Alien_color:
+    print('10 points earned')
+elif  'red'in Alien_color:
+    print('15 points earned')
+
+#exercise 5.6
+age=30
+if age <2:
+    print("You are a baby")
+elif age <4:
+    print('You are a Toddler')
+elif age <13:
+    print('You are a Kid')
+elif age <20:
+    print('You are a Teenager')
+elif age <65:
+    print('You are an Elder')
+
+#exercise 5.7
+favourite_fruits=['Apples', 'Watermelon', 'Orange', 'Banana']
+if 'Orange' in favourite_fruits:
+    print('You really like Oranges')
